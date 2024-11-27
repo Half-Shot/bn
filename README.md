d80ed41eb8c2e5bf408681f01243e17ae2ec4996
Will Hunt <will@half-shot.uk>
2024-11-27 14:28:08 +0000

    Add contrib bits

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # bn
+
+Simple Rust application to check the battery level and send a notification when it drops too low.
+
+This works best as a Systemd user service which runs on a timer. An example is provided in the `contrib` directory.
+
+```
+Simple application to notify when the battery drops too low
+
+Usage: bn [OPTIONS] --critical-percentage <CRITICAL_PERCENTAGE>
+
+Options:
+  -w, --warn-percentage <WARN_PERCENTAGE>
+          When the battery drops below this level, send a warning notification
+  -c, --critical-percentage <CRITICAL_PERCENTAGE>
+          When the battery drops below this level, send an urgent critical notification
+  -s, --serial <SERIAL>
+          The serial number of the battery to check. If not provided, this command will list all batteries
+  -h, --help
+          Print help
+  -V, --version
+          Print version
+```
+
+## Building
+
+```sh
+cargo build -r
+./target/release/bn -s 
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, fs, io::Read, time::Duration};
+use std::{env, fs, io::Read, path::PathBuf, time::Duration};
 
+use battery::Batteries;
 use clap::Parser;
 use notify_rust::{Notification, Urgency};
 
@@ -13,12 +14,62 @@ struct Args {
     /// When the battery drops below this level, send an urgent critical notification.
     #[arg(short, long)]
     critical_percentage: u32,
-    /// The serial number of the battery to check
+    /// The serial number of the battery to check. If not provided, this command will list all batteries.
     #[arg(short, long)]
-    serial: String,
+    serial: Option<String>,
 }
 
 const POWER_STATE_FILE: &str = "bn_state";
+
+fn check_battery(
+    mut batteries: Batteries,
+    serial: String,
+    prev_value: u32,
+    power_state_path: PathBuf,
+    critical_percentage: u32,
+    warn_percentage: Option<u32>,
+) {
+    match batteries.find(|b| {
+        b.as_ref()
+            .is_ok_and(|f| f.serial_number().is_some_and(|f| serial.eq(f)))
+    }) {
+        Some(Ok(battery)) => {
+            let percentage = (battery.state_of_charge().value * 100.0).floor() as u32;
+            fs::write(power_state_path, u32::to_le_bytes(percentage))
+                .expect("Failed to write power value to file");
+
+            if battery.time_to_full().is_some() {
+                // Charging, skip.
+            }
+
+            println!("prev: {:?}, curr: {:?}", prev_value, percentage);
+
+            if percentage <= critical_percentage && prev_value > critical_percentage {
+                Notification::new()
+                    .summary("Battery level")
+                    .body(format!("Battery level is CRITICAL ({:?}%)", percentage).as_str())
+                    .hint(notify_rust::Hint::Urgency(Urgency::Critical))
+                    .hint(notify_rust::Hint::SoundName("battery-caution".to_string()))
+                    .show()
+                    .expect("Failed to show notif");
+            } else if warn_percentage.is_some_and(|f| percentage <= f && prev_value > f) {
+                Notification::new()
+                    .summary("Battery level")
+                    .body(format!("Battery level is low ({:?}%)", percentage).as_str())
+                    .hint(notify_rust::Hint::Urgency(Urgency::Normal))
+                    .timeout(Duration::from_secs(30))
+                    .show()
+                    .expect("Failed to show notif");
+            }
+        }
+        Some(Err(e)) => {
+            eprintln!("Unable to access battery information {:?}", e);
+        }
+        None => {
+            eprintln!("Unable to find any batteries");
+        }
+    }
+}
 
 fn main() {
     let args = Args::parse();
@@ -35,50 +86,27 @@ fn main() {
         .unwrap_or(100);
 
     let manager = battery::Manager::new().expect("Unable to start battery manager");
-    match manager
-        .batteries()
-        .expect("Unable to read batteries")
-        .find(|b| {
-            b.as_ref()
-                .is_ok_and(|f| f.serial_number().is_some_and(|f| args.serial.eq(f)))
-        }) {
-        Some(Ok(battery)) => {
-            let percentage = (battery.state_of_charge().value * 100.0).floor() as u32;
-            fs::write(power_state_path, u32::to_le_bytes(percentage))
-                .expect("Failed to write power value to file");
+    let batteries = manager.batteries().expect("Unable to read batteries");
 
-            if battery.time_to_full().is_some() {
-                // Charging, skip.
-            }
-
-            println!("prev: {:?}, curr: {:?}", prev_value, percentage);
-
-            if percentage <= args.critical_percentage && prev_value > args.critical_percentage {
-                Notification::new()
-                    .summary("Battery level")
-                    .body(format!("Battery level is CRITICAL ({:?}%)", percentage).as_str())
-                    .hint(notify_rust::Hint::Urgency(Urgency::Critical))
-                    .hint(notify_rust::Hint::SoundName("battery-caution".to_string()))
-                    .show()
-                    .expect("Failed to show notif");
-            } else if args
-                .warn_percentage
-                .is_some_and(|f| percentage <= f && prev_value > f)
-            {
-                Notification::new()
-                    .summary("Battery level")
-                    .body(format!("Battery level is low ({:?}%)", percentage).as_str())
-                    .hint(notify_rust::Hint::Urgency(Urgency::Normal))
-                    .timeout(Duration::from_secs(30))
-                    .show()
-                    .expect("Failed to show notif");
+    if let Some(battery_serial) = args.serial {
+        check_battery(
+            batteries,
+            battery_serial,
+            prev_value,
+            power_state_path,
+            args.critical_percentage,
+            args.warn_percentage,
+        );
+    } else {
+        println!("No serial given, listing possible batteries");
+        for possible_battery in batteries {
+            if let Ok(battery) = possible_battery {
+                println!(
+                    " - {:?} (vendor: {:?})",
+                    battery.serial_number().or(Some("no id")).unwrap(),
+                    battery.vendor().or(Some("no vendor")).unwrap()
+                );
             }
         }
-        Some(Err(e)) => {
-            eprintln!("Unable to access battery information {:?}", e);
-        }
-        None => {
-            eprintln!("Unable to find any batteries");
-        }
-    };
+    }
 }
